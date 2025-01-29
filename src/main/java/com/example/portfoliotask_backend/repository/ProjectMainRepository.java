80b87c2ae4f3d54781c864c13a8cf6b7e4dfea42
pooja682002 <pooja.azhi@gmail.com>
2025-01-29 12:05:43 +0530

    Project Page Updated

--- src/main/java/com/example/portfoliotask_backend/repository/ProjectMainRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.portfoliotask_backend.repository;
-
-import com.example.portfoliotask_backend.model.ProjectMain;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface ProjectMainRepository extends JpaRepository<ProjectMain, UUID> {
-}
